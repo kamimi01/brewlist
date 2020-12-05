5e4313c54f8c1bba51255ed59fe8a0035404dd5d
kamimi01 <awakaru0122@gmail.com>
2020-12-05 22:52:04 +0900

    brew listの結果をマークダウンファイルに書き出すシェル完成

---
+++ createBrewList.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# homebrewにインストールしているソフトウェアの情報を出力するコマンドの実行
+# 一行ずつ取り出してマークダウンの表形式に出力し、README.mdに追記
+brew list --versions | while read -r line
+do
+  set ${line}
+  SOFTWARENAME=${1}
+  VERSION1=${2}
+  VERAION2=${3}
+  echo "| ${SOFTWARENAME} | ${VERSION1} |" >> README.md
+done
