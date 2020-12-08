9dc353bd736c0612ed499a4df99f9367bb1685b1
kamimi01 <awakaru0122@gmail.com>
2020-12-08 22:16:22 +0900

    update brew info

--- scripts/createBrewList.sh
+++ scripts/createBrewList.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# README.mdのディレクトリまで移動
+READMEDIR="/Users/mikaurakawa/Desktop/Shell_Projects/brewlist"
+cd $READMEDIR
+
+# README.mdファイルの行数を取得
+READMEFILENAME="/Users/mikaurakawa/Desktop/Shell_Projects/brewlist/README.md"
+declare -i NUMBEROFALLLINES
+NUMBEROFALLLINES=`cat ${READMEFILENAME} | wc -l`
+
+# 9行目以降を削除して、ファイルを上書き
+sed -i -e "9,${NUMBEROFALLLINES}d" ${READMEFILENAME}
+
 # homebrewにインストールしているソフトウェアの情報を出力するコマンドの実行
 # 一行ずつ取り出してマークダウンの表形式に出力し、README.mdに追記
 brew list --versions | while read -r line
@@ -11,11 +23,7 @@ do
   echo "| ${SOFTWARENAME} | ${VERSION1} |" >> README.md
 done
 
-BREWLISTDIR="/Users/mikaurakawa/Desktop/Shell_Projects/brewlist/scripts"
-COMMITMESSAGE="update brew info"
-
 # Githubにコミット&プッシュ
-cd $BREWLISTDIR
 git add .
 git commit -m "update brew info"
 git push origin main
